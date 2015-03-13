689f7ad297b5579f325303f1acba0759e008c6a9
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-13 01:05:35 -0700

    Functional game, few changes

---
+++ README.md
@@ -0,0 +1,5 @@
+Strike-Wiz is a fun addition game made for children. The goal of the game is to use
+the 9 digits in the purple box to match the number shown after rolling the dice. The
+dice roll simulation only puts out a random number between 2 and 12 (same as rolling
+  2 dices). The player who successfully make all the numbers in the purple box disappear
+  wins and is officially a Strike-Wiz. Enjoy.  
--- main.js
+++ main.js
@@ -11,13 +11,19 @@ var timer;
 
 function init() {
 
+  $('.output').html('Roll the dice and add the right numbers');
+
   var rolled = false;
+  $('.num').prop('disabled', true);
+  $('#answer').prop('disabled', true);
 
   $('.reset').click(function() {
+    $('.num').prop('disabled', true);
     rolled = false;
     $('#result').html("0");
     $('.screen').text('');
-    $('.keys').removeClass('selected');
+    $('.num').removeClass('selected');
+
     $('.num').val('');
     sum = 0;
   });
@@ -25,43 +31,55 @@ function init() {
   $('.roll').click(function() {
     rolled = true;
 
-    newDice = Math.floor(Math.random()*12) +1;
+    newDice = Math.floor(Math.random()*11) +2;
     diceRolls.push(newDice);
     total += diceRolls[diceRolls.length -1];
     $('.output').html('');
     $('#result').html(newDice);
+
+    $('.num').prop('disabled', false);
+    $('#answer').prop('disabled', false);
   });
-
-  $('.keys').on('click', '.num', function() {
-
-    $(this).addClass('selected');
-    var clickVal = $(this).text();
-    var screenVal = $('.screen').text();
-
-    console.log('clickVal: ' + clickVal);
-    console.log('screenVal: ' + screenVal);
-
-    var totalVal = screenVal + clickVal;
-
-    console.log('totalVal: ' + totalVal);
-    $('.screen').text(totalVal);
-    var inp = parseInt(clickVal);
-    sum += inp;
-    if (!clickVal) {
-      $('.output').html('Roll the dice and add the right numbers');
-    }
-  });
-
-  $('#answer').click(function() {
-    $('.screen').text('');
-    if (sum !== newDice) {
-      $('.output').html('Wrong addition, try again!');
-    } else {
-      $('.output').html('Good, roll again!');
-    }
-    $('#result').html("0");
-    sum = 0;
-
-  });
-
 }
+
+// $('.num').prop('disabled', true);
+$('.keys').on('click', '.num', function() {
+
+  $(this).addClass('selected');
+  $(this).prop('disabled', true);
+  $('#answer').prop('disabled', false);
+
+  var clickVal = $(this).text();
+  var screenVal = $('.screen').text();
+
+  var totalVal = screenVal + clickVal;
+
+  $('.screen').text(totalVal);
+  var inp = parseInt(clickVal);
+  sum += inp;
+
+  if ($('#one').hasClass('selected') && $('#two').hasClass('selected') && $('#three').hasClass('selected')
+  && $('#four').hasClass('selected') && $('#five').hasClass('selected') && $('#six').hasClass('selected')
+  && $('#seven').hasClass('selected') && $('#eight').hasClass('selected') && $('#nine').hasClass('selected') && (sum === newDice)) {
+    $('.output').html('Congratulations, You are a Strike Wiz!');
+    $('.num').prop('disabled', true);
+    $('#answer').prop('disabled', true);
+    $('.roll').prop('disabled', true);
+  }
+});
+
+$('#answer').click(function() {
+  $('.screen').text('');
+
+  if (sum === newDice) {
+    $('.output').html('Good, roll again!');
+  } else {
+    $('.output').html('Wrong addition, try again! Roll!');
+    $('.num').removeClass('selected');
+    // $('.num').prop('disabled', true);
+  }
+  $('.num').prop('disabled', true);
+  $('#result').html("0");
+  sum = 0;
+
+});
